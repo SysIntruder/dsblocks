b0740a2d4a14fcd3b805b00951ca1567f92af8ee
rei <raihanryuichi@gmail.com>
2025-06-25 06:31:30 +0700

    update signaler.c

--- signaler.c
+++ signaler.c
@@ -4,16 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
+  if (argc != 4) {
     return 1;
   }
 
-  pid_t pid = atoi(argv[1]);
+  int signum = atoi(argv[1]);
   int val = atoi(argv[2]);
+  pid_t pid = atoi(argv[3]);
 
   union sigval value;
   value.sival_int = val;
 
-  sigqueue(pid, SIGRTMIN, value);
+  sigqueue(pid, SIGRTMIN + signum, value);
   return 0;
 }
